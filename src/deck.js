a9421cfac995d92846599d59f134270ba9fb3692
leanneblack <leanne.black@cox.net>
2019-06-03 10:26:30 -0700

    put files in order

---
+++ src/deck.js
@@ -0,0 +1,41 @@
+function shuffle(array) {
+  const _array = array.slice(0)
+  for (var i = _array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = _array[i]
+    _array[i] = _array[j]
+    _array[j] = temp
+  }
+
+  return _array
+}
+
+export default function initializeDeck() {
+  let id = 0
+  const cards = [
+    'andy',
+    'ann',
+    'Ben',
+    'April',
+    'chris',
+    'donna',
+    'leslie',
+    'ron'
+  ].reduce((acc, type) => {
+    acc.push(
+      ...[
+        {
+          id: id++,
+          type
+        },
+        {
+          id: id++,
+          type
+        }
+      ]
+    )
+    return acc
+  }, [])
+
+  return shuffle(cards)
+}
